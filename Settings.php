ac0f7a8756bfa711e02aae474063a3b930b6c8a4
Philippe Signoret <philippe.signoret@gmail.com>
2014-02-15 04:28:41 -0800

    made field UPN is matched against configurable

--- Settings.php
+++ Settings.php
@@ -11,8 +11,8 @@ class AADSSO_Settings {
 
 	// The client ID, get this from AAD
 	//public $clientId = 'e69981fb-edab-4385-944d-03c464ad41ec';   // philippesignoretoutlook.onmicrosoft.com Not native
-	public $clientId = '7be797bb-fc35-43c9-826c-2f9e956f6ca4';   // philippesignoretoutlook.onmicrosoft.com Native client app
-	//public $clientId = 'f68a1908-9fa7-4e22-9600-594c2cf625bd';   // transeconomics.com Native client app
+	//public $clientId = '7be797bb-fc35-43c9-826c-2f9e956f6ca4';   // philippesignoretoutlook.onmicrosoft.com Native client app
+	public $clientId = 'f68a1908-9fa7-4e22-9600-594c2cf625bd';   // transeconomics.com Native client app
 	    
 	// Don't need it! :)
 	public $password = '';
@@ -21,7 +21,7 @@ class AADSSO_Settings {
 	public $redirectURI =         '';
 
 	// Logout reply URL, doesn't need to be configured anywhere
-	public $logoutRedirectURI =   'https://psignoret-oauth.azurewebsites.net/Logout.php';
+	public $logoutRedirectURI =   '';
 
 	// The display name of the organization
 	public $org_display_name = 'TransEconomics';
