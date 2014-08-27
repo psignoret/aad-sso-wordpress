7c04f34f7173272132ba494474b4f1a564e990c6
Justin Sternberg <justin@dsgnwrks.pro>
2014-08-27 18:39:41 -0400

    looks like com_create_guid is more commonly available in Windows, so add a fallback

--- aad-sso-wordpress.php
+++ aad-sso-wordpress.php
@@ -197,3 +197,20 @@ if ( ! file_exists( AADSSO_SETTINGS_PATH ) ) {
 	$settings = AADSSO_Settings::loadSettingsFromJSON(AADSSO_SETTINGS_PATH);
 	$aadsso = AADSSO::getInstance($settings);
 }
+
+
+ if ( ! function_exists( 'com_create_guid' ) ) {
+	function com_create_guid(){
+		mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+		$charid = strtoupper(md5(uniqid(rand(), true)));
+		$hyphen = chr(45);// "-"
+		$uuid = chr(123)// "{"
+			.substr($charid, 0, 8).$hyphen
+			.substr($charid, 8, 4).$hyphen
+			.substr($charid,12, 4).$hyphen
+			.substr($charid,16, 4).$hyphen
+			.substr($charid,20,12)
+			.chr(125);// "}"
+		return $uuid;
+	}
+ }
