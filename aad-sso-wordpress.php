340f27e82e52e3059b98b3ab1275e0032526d547
Justin Sternberg <justin@dsgnwrks.pro>
2014-08-27 18:44:03 -0400

    WP coding standards

--- aad-sso-wordpress.php
+++ aad-sso-wordpress.php
@@ -207,9 +207,9 @@ if ( ! function_exists( 'com_create_guid' ) ) {
 		$uuid = chr( 123 ) . // "{"
 			substr( $charid, 0, 8 ) . $hyphen .
 			substr( $charid, 8, 4 ) . $hyphen .
-			substr( $charid,12, 4 ) . $hyphen .
-			substr( $charid,16, 4 ) . $hyphen .
-			substr( $charid,20,12 ) .
+			substr( $charid, 12, 4 ) . $hyphen .
+			substr( $charid, 16, 4 ) . $hyphen .
+			substr( $charid, 20, 12 ) .
 			chr( 125 );// "}"
 
 		return $uuid;
