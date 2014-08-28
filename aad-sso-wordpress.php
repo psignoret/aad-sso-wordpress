7724f6fee6ab7dcf3078a33b5496c9fa0b37b3b3
Justin Sternberg <justin@dsgnwrks.pro>
2014-08-28 14:55:13 -0400

    a few more standards tweaks

--- aad-sso-wordpress.php
+++ aad-sso-wordpress.php
@@ -203,14 +203,14 @@ if ( ! function_exists( 'com_create_guid' ) ) {
 	function com_create_guid(){
 		mt_srand( (double) microtime() * 10000 ); //optional for php 4.2.0 and up.
 		$charid = strtoupper( md5( uniqid( rand(), true ) ) 	);
-		$hyphen = chr( 45 );// "-"
+		$hyphen = chr( 45 ); // "-"
 		$uuid = chr( 123 ) . // "{"
 			substr( $charid, 0, 8 ) . $hyphen .
 			substr( $charid, 8, 4 ) . $hyphen .
 			substr( $charid, 12, 4 ) . $hyphen .
 			substr( $charid, 16, 4 ) . $hyphen .
 			substr( $charid, 20, 12 ) .
-			chr( 125 );// "}"
+			chr( 125 ); // "}"
 
 		return $uuid;
 	}
