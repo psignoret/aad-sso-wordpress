b508db3ecdcd0dcde1a9c92e4ec893986ba0ef64
blobaugh <ben@lobaugh.net>
2014-09-09 17:36:14 -0700

    Stubbing Settings page
    Settings are currently stored in a json file. This is annoying for two reasons:
    The file is world readable (whoops), Site admins cannot change settings from WP

--- Settings.php
+++ Settings.php
@@ -115,7 +115,14 @@ class AADSSO_Settings {
 	 */
 	public $graphVersion =          '2013-11-08';
 
-	public function __construct () {}
+	public function __construct() {
+		
+		if( is_admin() ) {
+			// Setup stuff only needed in wp-admin
+			add_action( 'admin_menu', array( $this, 'add_menus' ) );
+			add_action( 'admin_init', array( $this, 'register_settings' ) );
+		}
+	}
 
 	/**
 	 * @return self The (only) instance of the class.
@@ -149,9 +156,17 @@ class AADSSO_Settings {
 
 	function importSettingsFromJSON($jsonFile) {
 		// Load the JSON settings
-		$jsonSettings = file_get_contents($jsonFile);
+		//$jsonSettings = file_get_contents($jsonFile);
+		if( file_exists( $jsonFile ) ) {
+			$f = fopen( $jsonFile, "r" ) or die( "Unable to open settings file" );
+			$jsonSettings = fread( $f, filesize( $jsonFile ) );
+			fclose( $f );
+		} else {
+			$response = wp_remote_get( $jsonFile );
+			$jsonSettings = wp_remote_retrieve_body( $response );
+		}
 		$tmpSettings = json_decode($jsonSettings, TRUE);
-
+		
 		// Overwrite any properties defined in the JSON
 		foreach ($tmpSettings as $key => $value) {
 			if (property_exists($this, $key)) {
@@ -160,5 +175,153 @@ class AADSSO_Settings {
 		}
 
 		return $this;
+	}
+
+	public function add_menus() {
+		add_options_page( 'AAD Settings', 'AAD Settings', 'manage_options', 'aad-settings', array( $this, 'render_admin_settings' ) );
+	}
+
+	public function render_admin_settings() {
+		require_once( 'views/admin/settings.php' );
+	}
+
+	public function register_settings() {
+		
+		register_setting( 'aad-settings', 'aad-settings' );
+
+		/*
+		 * Directory Settings
+		 * - Org name
+		 * - Org domain
+		 * - Secret keys
+		 */
+		add_settings_section( 
+			'aad-directory-settings', 
+			__( 'Directory Settings' ), 
+			array( $this, 'render_directory_settings_section' ), 
+			'aad-settings' 
+		);
+
+		add_settings_field( 
+			'org_display_name',
+			__( 'Organization Display Name' ),
+			array( $this, 'render_org_display_name' ),
+			'aad-settings',
+			'aad-directory-settings'
+		);
+
+		add_settings_field(
+			'org_domain_hint',
+			__( 'Organization Domain Hint' ),
+			array( $this, 'render_org_domain_hint' ),
+			'aad-settings',
+			'aad-directory-settings'
+		);
+		
+		add_settings_field(
+			'client_id',
+			__( 'Client ID' ),
+			array( $this, 'render_client_id' ),
+			'aad-settings',
+			'aad-directory-settings'
+		);
+
+		add_settings_field(
+			'client_secret',
+			__( 'Client Secret' ),
+			array( $this, 'render_client_secret' ),
+			'aad-settings',
+			'aad-directory-settings'
+		);
+		/*
+		 * Map of group hash from Azure to local groups
+		 */
+		add_settings_section( 
+			'aad-group-settings', 
+			__( 'Group Map' ), 
+			array( $this, 'render_group_settings_section' ), 
+			'aad-settings' 
+		);
+
+		add_settings_field(
+			'group_map_admin',
+			__( 'Administrator' ),
+			array( $this, 'render_group_map_admin' ),
+			'aad-settings',
+			'aad-group-settings'
+		);
+
+		add_settings_field(
+			'group_map_editor',
+			__( 'Editor' ),
+			array( $this, 'render_group_map_admin' ),
+			'aad-settings',
+			'aad-group-settings'
+		);
+
+		add_settings_field(
+			'group_map_author',
+			__( 'Author' ),
+			array( $this, 'render_group_map_author' ),
+			'aad-settings',
+			'aad-group-settings'
+		);
+
+		add_settings_field(
+			'group_map_contributor',
+			__( 'Contributor' ),
+			array( $this, 'render_group_map_contributor' ),
+			'aad-settings',
+			'aad-group-settings'
+		);
+
+		add_settings_field( 
+			'group_map_subscriber',
+			__( 'Subscriber' ),
+			array( $this, 'render_group_map_subscriber' ),
+			'aad-settings',
+			'aad-group-settings'
+		);
+	}
+
+	public function render_directory_settings_section() {}
+
+	public function render_org_display_name() {
+		$settings = get_option( 'aad-settings' );
+		var_dump( $settings );
+	}
+
+	public function render_org_domain_hint() {
+		echo 'domain hint';
+	}
+
+	public function render_client_id() {
+		echo 'client id';
+	}
+
+	public function render_client_secret() {
+		echo 'client secret';
+	}
+
+	public function render_group_settings_section() {}
+
+	public function render_group_map_admin() {
+		echo 'admin';
+	}
+
+	public function render_group_map_editor() {
+		echo 'editor';
+	}
+
+	public function render_group_map_author() {
+		echo 'author';
+	}
+
+	public function render_group_map_contributor() {
+		echo 'contributor';
+	}
+
+	public function render_group_map_subscriber() {
+		echo 'subscriber';
 	}
 }
