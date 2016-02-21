f28096500e0d24b3ccea25bb1c5da3694fb8fe51
Philippe Signoret <philippe.signoret@outlook.com>
2016-02-21 09:42:05 -0800

    General cleanup and alignment with WP coding style
    Normalizes whitespace, replaces use of camelCase, removes unused methods.

--- AuthorizationHelper.php
+++ AuthorizationHelper.php
@@ -1,118 +1,149 @@
 <?php
 
 /**
- * A class used to request authorization and access tokens Azure Active Directory.
+ * A helper class used to request authorization and access tokens Azure Active Directory.
  */
 class AADSSO_AuthorizationHelper
 {
-	// Currently, only RS256 is allowed and expected from AAD.
+	/**
+	 * @var string List of allowed algorithms. Currently, only RS256 is allowed and expected from AAD.
+	 */
 	private static $allowed_algorithms = array('RS256');
 
-	// Get the authorization URL which makes the authorization request
-	public static function getAuthorizationURL($settings, $antiforgery_id) {
-		$authUrl = $settings->authorization_endpoint . '?' .
-				   http_build_query(
-						array(
-							'response_type' => 'code',
-							'scope' => 'openid',
-							'domain_hint' => $settings->org_domain_hint,
-							'client_id' => $settings->client_id,
-							'resource' => $settings->resourceURI,
-							'redirect_uri' => $settings->redirect_uri,
-							'state' => $antiforgery_id,
-							'nonce' => $antiforgery_id,
-						)
-				   );
-		return $authUrl;
+	/**
+	 * Gets the authorization URL used to makes the authorization request.
+	 *
+	 * @param \AADSSO_Settings $settings The settings to use.
+	 * @param string $antiforgery_id The value to use as the nonce.
+	 *
+	 * @return string The authorization URL.
+	 */
+	public static function get_authorization_url( $settings, $antiforgery_id ) {
+		$auth_url = $settings->authorization_endpoint . '?'
+		 . http_build_query( array(
+					'response_type' => 'code',
+					'scope'         => 'openid',
+					'domain_hint'   => $settings->org_domain_hint,
+					'client_id'     => $settings->client_id,
+					'resource'      => $settings->graph_endpoint,
+					'redirect_uri'  => $settings->redirect_uri,
+					'state'         => $antiforgery_id,
+					'nonce'         => $antiforgery_id,
+				) );
+		return $auth_url;
 	}
 
-	// Takes an authorization code and obtains an gets an access token
-	public static function getAccessToken($code, $settings) {
+
+	/**
+	 * Exchanges an Authorization Code and obtains an Access Token and an ID Token.
+	 *
+	 * @param string $code The authorization code.
+	 * @param \AADSSO_Settings $settings The settings to use.
+	 *
+	 * @return mixed The decoded authorization result.
+	 */
+	public static function get_access_token( $code, $settings ) {
 
 		// Construct the body for the access token request
-		$authenticationRequestBody = http_build_query(
-										array(
-											'grant_type' => 'authorization_code',
-											'code' => $code,
-											'redirect_uri' => $settings->redirect_uri,
-											'resource' => $settings->resourceURI,
-											'client_id' => $settings->client_id,
-											'client_secret' => $settings->client_secret
-										)
-									);
+		$authentication_request_body = http_build_query(
+			array(
+				'grant_type'    => 'authorization_code',
+				'code'          => $code,
+				'redirect_uri'  => $settings->redirect_uri,
+				'resource'      => $settings->graph_endpoint,
+				'client_id'     => $settings->client_id,
+				'client_secret' => $settings->client_secret
+			)
+		);
 
-		return self::getAndProcessAccessToken($authenticationRequestBody, $settings);
+		return self::get_and_process_access_token( $authentication_request_body, $settings );
 	}
 
-	// Makes the request for the access token and some does some basic processing of the result
-	static function getAndProcessAccessToken($authenticationRequestBody, $settings) {
+	/**
+	 * Makes the request for the access token and some does some basic processing of the result.
+	 *
+	 * @param array $authentication_request_body The body to use in the Authentication Request.
+	 * @param \AADSSO_Settings $settings The settings to use.
+	 *
+	 * @return mixed The decoded authorization result.
+	 */
+	public static function get_and_process_access_token( $authentication_request_body, $settings ) {
 
-		// Post the authorization code to the STS and get back the access token 
-		$response = wp_remote_post($settings->token_endpoint, array(
-			'body' => $authenticationRequestBody
-		));
+		// Post the authorization code to the STS and get back the access token
+		$response = wp_remote_post( $settings->token_endpoint, array(
+			'body' => $authentication_request_body
+		) );
 		if( is_wp_error( $response ) ) {
-			return new WP_Error($response->get_error_code(), $response->get_error_message() );
+			return new WP_Error( $response->get_error_code(), $response->get_error_message() );
 		}
-		$output = wp_remote_retrieve_body($response);
+		$output = wp_remote_retrieve_body( $response );
 
-		// Decode the JSON response from the STS. If all went well, this will contain the access 
+		// Decode the JSON response from the STS. If all went well, this will contain the access
 		// token and the id_token (a JWT token telling us about the current user)
-		$token = json_decode($output);
+		$result = json_decode( $output );
 
-		if ( isset($token->access_token) ) {
+		if ( isset( $result->access_token ) ) {
 
 			// Add the token information to the session so that we can use it later
 			// TODO: these probably shouldn't be in SESSION...
-			$_SESSION['token_type'] = $token->token_type;
-			$_SESSION['access_token'] = $token->access_token;
+			$_SESSION['token_type'] = $result->token_type;
+			$_SESSION['access_token'] = $result->access_token;
 		}
 
-		return $token;
+		return $result;
 	}
 
-	public static function validateIdToken($id_token, $settings, $antiforgery_id) {
+	/**
+	 * Decodes and validates an id_token value returned
+	 *
+	 * @param array $authentication_request_body The body to use in the Authentication Request.
+	 * @param \AADSSO_Settings $settings The settings to use.
+	 *
+	 * @return mixed The decoded authorization result.
+	 */
+	public static function validate_id_token( $id_token, $settings, $antiforgery_id ) {
 
-		$jwt = NULL;
-		$lastException = NULL;
+		$jwt = null;
+		$last_exception = null;
 
 		// TODO: cache the keys
-		$discovery = json_decode(file_get_contents($settings->jwks_uri));
+		$discovery = json_decode( file_get_contents( $settings->jwks_uri ) );
 
-		if ($discovery->keys == NULL) {
-			throw new DomainException('jwks_uri does not contain the keys attribute');
+		if ( null == $discovery->keys ) {
+			throw new DomainException( 'jwks_uri does not contain the keys attribute' );
 		}
 
-		foreach ($discovery->keys as $key) {
+		foreach ( $discovery->keys as $key ) {
 			try {
-				if ($key->x5c == NULL) {
+				if ( null == $key->x5c ) {
 					throw new DomainException('key does not contain the x5c attribute');
 				}
 
 				$key_der = $key->x5c[0];
 
-				// Per section 4.7 of the current JWK draft [1], the 'x5c' property will be the 
-				// DER-encoded value of the X.509 certificate. PHP's openssl functions all require 
-				// a PEM-encoded value.
-				$key_pem = chunk_split($key_der, 64, "\n");
+				/* Per section 4.7 of the current JWK draft [1], the 'x5c' property will be the
+				 * DER-encoded value of the X.509 certificate. PHP's openssl functions all require
+				 * a PEM-encoded value.
+				 */
+				$key_pem = chunk_split( $key_der, 64, "\n" );
 				$key_pem = "-----BEGIN CERTIFICATE-----\n"
 				            . $key_pem
-				            ."-----END CERTIFICATE-----\n";
+				            . "-----END CERTIFICATE-----\n";
 
-				// This throws ab exception if the id_token cannot be validated.
-				$jwt = JWT::decode($id_token, $key_pem, self::$allowed_algorithms);
+				// This throws an exception if the id_token cannot be validated.
+				$jwt = JWT::decode( $id_token, $key_pem, self::$allowed_algorithms );
 				break;
-			} catch (Exception $e) {
-				$lastException = $e;
+			} catch ( Exception $e ) {
+				$last_exception = $e;
 			}
 		}
 
-		if ($jwt == NULL) {
-			throw $lastException;
+		if ( null == $jwt ) {
+			throw $last_exception;
 		}
 
 		if ($jwt->nonce != $antiforgery_id) {
-			throw new DomainException(sprintf('Nonce mismatch. Expecting %s', $antiforgery_id));
+			throw new DomainException( sprintf( 'Nonce mismatch. Expecting %s', $antiforgery_id ) );
 		}
 
 		return $jwt;
--- GraphHelper.php
+++ GraphHelper.php
@@ -1,115 +1,106 @@
 <?php
 
 /**
- * A class used to make calls to Azure Active Directory Graph API.
+ * A helper class used to make calls to Azure Active Directory Graph API.
  */
 class AADSSO_GraphHelper
 {
-	public static $ch;
+	/**
+	 * @var \AADSSO_Settings The instance of AADSSO_Settings to use.
+	 */
 	public static $settings;
-	public static $token;
+
+	/**
+	 * @var string The tenant ID against which Graph API calls will be made.
+	 */
 	public static $tenant_id;
 
-	// TODO: validation if tenant_id is not set
-	public static function getResourceUrl() {
-		return self::$settings->resourceURI . '/' . self::$tenant_id;
+	/**
+	 * Gets the the Azure AD Graph API to use.
+	 *
+	 * @return string The base URL to the Azure AD Graph API.
+	 */
+	public static function get_resource_url() {
+		if ( null == self::$tenant_id ) {
+			throw new DomainException( 'Missing tenant ID for making Azure AD Graph API calls.' );
+		}
+		return self::$settings->graph_endpoint . '/' . self::$tenant_id;
 	}
 
-	public static function getUsers() {
-		$url = self::getResourceUrl() . '/users/';
-		return self::getRequest($url);
+	/**
+	 * Checks which of the given groups the given user is a member of.
+	 *
+	 * @return mixed The response to the checkMemberGroups request.
+	 */
+	public static function user_check_member_groups( $id, $group_ids ) {
+		$url = self::get_resource_url() . '/users/' . $id . '/checkMemberGroups';
+		return self::post_request( $url, array(), array( 'groupIds' => $group_ids ) );
 	}
 
-	public static function getUserMemberOf($id) {
-		$url = self::getResourceUrl() . '/users/' . $id . '/$links/memberOf';
-		return self::getRequest($url);
+	/**
+	 * Gets the requested user.
+	 *
+	 * @return mixed The response to the user request.
+	 */
+	public static function get_user( $id ) {
+		$url = self::get_resource_url() . '/users/' . $id;
+		return self::get_request( $url );
 	}
 
-	public static function userCheckMemberGroups($id, $group_ids) {
-		$url = self::getResourceUrl() . '/users/' . $id . '/checkMemberGroups';
-		return self::postRequest($url, array(), array('groupIds' => $group_ids));
-	}
+	/**
+	 * Issues a GET request to the Azure AD Graph API.
+	 *
+	 * @return mixed The decoded response.
+	 */
+	public static function get_request( $url, $query_params = array() ) {
 
-	public static function getUser($id) {
-		$url = self::getResourceUrl() . '/users/' . $id;
-		return self::getRequest($url);
-	}
-
-	public static function updateUser($id, $data) {
-		$url = self::getResourceUrl() . '/users/' . $id;
-		return self::patchRequest($url, array(), $data);
-	}
-
-	public static function getMe() {
-		$url = self::getResourceUrl() . '/me';
-		return self::getRequest($url);
-	}
-
-	public static function updateMe($data) {
-		$url = self::getResourceUrl() . '/me';
-		return self::patchRequest($url, array(), $data);
-	}
-
-	public static function getRequest($url, $query_params = array()) {
-
-		// Build the full query URL
-		$query_params = http_build_query(self::maybeAddApiVersion($query_params));
+		// Build the full query URL, adding api-version if necessary
+		$query_params = http_build_query( self::maybe_add_api_version( $query_params ) );
 		$url = $url . '?' . $query_params;
 
-		$_SESSION['last_request'] = array('method' => 'GET', 'url' => $url);
+		$_SESSION['last_request'] = array(
+			'method' => 'GET',
+			'url' => $url,
+		);
 
 		// Make the GET request
-		$response = wp_remote_get($url, array(
-			'headers' => self::GetRequiredHeadersAndSettings(),
-		));
+		$response = wp_remote_get( $url, array(
+			'headers' => self::get_required_headers_and_settings(),
+		) );
 
 		// Parse the response
-		$decoded_output = json_decode(wp_remote_retrieve_body($response));
-		
+		$decoded_output = json_decode( wp_remote_retrieve_body( $response ) );
+
 		$_SESSION['last_request']['response'] = $decoded_output;
 		return $decoded_output;
 	}
 
-	public static function patchRequest($url, $query_params = array(), $data = array()) {
+	/**
+	 * Issues a POST request to the Azure AD Graph API.
+	 *
+	 * @return mixed The decoded response.
+	 */
+	public static function post_request( $url, $query_params = array(), $data = array() ) {
 
 		// Build the full query URL and encode the payload
-		$query_params = http_build_query(self::maybeAddApiVersion($query_params));
+		$query_params = http_build_query( self::maybe_add_api_version( $query_params ) );
 		$url = $url . '?' . $query_params;
 		$payload = json_encode($data);
 
-		$_SESSION['last_request'] = array('method' => 'PATCH', 'url' => $url, 'body' => $payload);
-		
-		// Make the PATCH request
-		$response = wp_remote_request($url, array(
-			'method' => 'PATCH',
+		$_SESSION['last_request'] = array(
+			'method' => 'POST',
+			'url' => $url,
 			'body' => $payload,
-			'headers' => self::GetRequiredHeadersAndSettings(),
-		));
+		);
 
-		// Parse the response
-		$decoded_output = json_decode(wp_remote_retrieve_body($response));
-
-		$_SESSION['last_request']['response'] = $decoded_output;
-		return $decoded_output;
-	}
-
-	public static function postRequest($url, $query_params = array(), $data = array()) {
-
-		// Build the full query URL and encode the payload
-		$query_params = http_build_query(self::maybeAddApiVersion($query_params));
-		$url = $url . '?' . $query_params;
-		$payload = json_encode($data);
-
-		$_SESSION['last_request'] = array('method' => 'POST', 'url' => $url, 'body' => $payload);
-		
 		// Make the POST request
-		$response = wp_remote_post($url, array(
+		$response = wp_remote_post( $url, array(
 			'body' => $payload,
-			'headers' => self::GetRequiredHeadersAndSettings(),
-		));
+			'headers' => self::get_required_headers_and_settings(),
+		) );
 
 		// Parse the response
-		$decoded_output = json_decode(wp_remote_retrieve_body($response));
+		$decoded_output = json_decode( wp_remote_retrieve_body( $response ) );
 
 		$_SESSION['last_request']['response'] = $decoded_output;
 		return $decoded_output;
@@ -117,31 +108,31 @@ class AADSSO_GraphHelper
 
 	/**
 	 * Adds the AAD Graph API version from settings, if not present already.
-	 * 
-	 * @param Array $query_params The associative array of query parameters. 
-	 * @return Array An associative array of query parameters, including api-version.
+	 *
+	 * @param array $query_params The associative array of query parameters.
+	 *
+	 * @return array An associative array of query parameters, including api-version.
 	 */
-	public static function maybeAddApiVersion($query_params) {
-		if ( ! array_key_exists('api-version', $query_params) ) {
-			$query_params['api-version'] = self::$settings->graphVersion;
+	public static function maybe_add_api_version( $query_params ) {
+		if ( ! array_key_exists( 'api-version', $query_params ) ) {
+			$query_params['api-version'] = self::$settings->graph_version;
 		}
 		return $query_params;
 	}
 
 	/**
 	  * Returns an array with the required headers like authorization header, service version etc.
-	  * 
+	  *
 	  * @return array An associative array with the HTTP headers for AAD Graph API calls.
 	  */
-	public static function GetRequiredHeadersAndSettings()
+	public static function get_required_headers_and_settings()
 	{
 		// Generate the authentication header
 		return array(
 			'Authorization' => $_SESSION['token_type'] . ' ' . $_SESSION['access_token'],
-			'Accept' => 'application/json;odata=minimalmetadata',
-			'Content-Type' => 'application/json;odata=minimalmetadata',
-			'Prefer' => 'return-content',
+			'Accept'        => 'application/json;odata=minimalmetadata',
+			'Content-Type'  => 'application/json;odata=minimalmetadata',
+			'Prefer'        => 'return-content',
 		);
 	}
-
 }
