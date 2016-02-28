76bacbf51cade8f856fcef5239b3ab6613a0fad6
Brad Kovach <bradkovach@gmail.com>
2016-02-28 20:33:53 +0000

    fixed all missed esc_html__() on view/settings.php

--- view/settings.php
+++ view/settings.php
@@ -1,7 +1,7 @@
 <div class="wrap">
 
-	<h2><?php echo esc_html__('Single Sign-on with Azure Active Directory', AADSSO); ?></h2>
-	<p><?php echo esc_html__('Settings for configuring single sign-on with Azure Active Directory can be configured here.', AADSSO); ?></p>
+	<h2><?php echo esc_html__( 'Single Sign-on with Azure Active Directory' , AADSSO ); ?></h2>
+	<p><?php echo esc_html__( 'Settings for configuring single sign-on with Azure Active Directory can be configured here.' , AADSSO ); ?></p>
 
 	<form method="post" action="options.php">
 		<?php
@@ -12,7 +12,7 @@
 	</form>
 
 	<h3><?php echo esc_html__( 'Reset Plugin' , AADSSO ); ?></h3>
-	<p><?php echo __( 'Resetting the plugin will completely remove all settings.' , AADSSO ); ?></p>
+	<p><?php echo esc_html__( 'Resetting the plugin will completely remove all settings.' , AADSSO ); ?></p>
 	<p>
 		<?php
 		printf(
@@ -30,12 +30,12 @@
 	<?php if( defined( 'AADSSO_SETTINGS_PATH' ) && file_exists( AADSSO_SETTINGS_PATH ) ): ?>
 		<h3><?php echo esc_html__( 'Migrate Legacy Settings', AADSSO ); ?></h3>
 		<p><?php printf(
-			__( 'Old configuration data was found at %s.' , AADSSO ),
+			esc_html__( 'Old configuration data was found at %s.' , AADSSO ),
 			sprintf( __( '<code>%s</code>' , AADSSO ), esc_html( AADSSO_SETTINGS_PATH ) )
 		); ?>  
 			<?php echo esc_html__( 'This configuration data can be migrated automatically.' , AADSSO ); ?></p>
 		<p><?php printf(
-				__( 'Delete the file at %s or unset the %s constant to hide this migration utility.' , AADSSO ),
+				esc_html__( 'Delete the file at %s or unset the %s constant to hide this migration utility.' , AADSSO ),
 				sprintf( __( '<code>%s</code>', AADSSO ) , esc_html( AADSSO_SETTINGS_PATH ) ),
 				sprintf( __( '<code>%s</code>', AADSSO ) , esc_html( 'AADSSO_SETTINGS_PATH' ) )
 			); ?></p>
