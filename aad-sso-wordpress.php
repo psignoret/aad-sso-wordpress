a095d52c10f4c307f9f40ff87e7ffce02cc248fc
Ryan Welcher <ryan.welcher@10up.com>
2018-02-11 08:25:38 -0500

    Ensuring that the plugin is configured to check for groups before limiting user creation

--- aad-sso-wordpress.php
+++ aad-sso-wordpress.php
@@ -291,14 +291,19 @@ class AADSSO {
 					);
 				}
 
-				// 1. Retrieve the Groups for this user once here so we can pass them around as needed.
-				// Pass the settings to GraphHelper
-				AADSSO_GraphHelper::$settings  = $this->settings;
-				AADSSO_GraphHelper::$tenant_id = $jwt->tid;
+				// Set a default value for group_memberships.
+				$group_memberships = false;
 
-				// Of the AAD groups defined in the settings, get only those where the user is a member
-				$group_ids         = array_keys( $this->settings->aad_group_to_wp_role_map );
-				$group_memberships = AADSSO_GraphHelper::user_check_member_groups( $jwt->oid, $group_ids );
+				if ( true === $this->settings->enable_aad_group_to_wp_role ) {
+					// 1. Retrieve the Groups for this user once here so we can pass them around as needed.
+					// Pass the settings to GraphHelper
+					AADSSO_GraphHelper::$settings  = $this->settings;
+					AADSSO_GraphHelper::$tenant_id = $jwt->tid;
+
+					// Of the AAD groups defined in the settings, get only those where the user is a member
+					$group_ids         = array_keys( $this->settings->aad_group_to_wp_role_map );
+					$group_memberships = AADSSO_GraphHelper::user_check_member_groups( $jwt->oid, $group_ids );
+				}
 
 
 				// Invoke any configured matching and auto-provisioning strategy and get the user.
@@ -375,9 +380,20 @@ class AADSSO {
 
 			// Since the user was authenticated with AAD, but not found in WordPress,
 			// need to decide whether to create a new user in WP on-the-fly, or to stop here.
-			// 3. If there are no groups for this user, we should not be creating it.
-			if ( true === $this->settings->enable_auto_provisioning && ! empty( $group_memberships->value ) ) {
+			if ( true === $this->settings->enable_auto_provisioning ) {
 
+				// 3. If we are configured to check, and there are no groups for this user, we should not be creating it.
+				if ( true === $this->settings->enable_aad_group_to_wp_role && ! empty( $group_memberships->value ) ) {
+					// The user was authenticated, but not found in WP and auto-provisioning is disabled
+					return new WP_Error(
+						'user_not_registered',
+						sprintf(
+							__( 'ERROR: The authenticated user \'%s\' does not have a group assignment for this site.',
+							'aad-sso-wordpress' ),
+							$unique_name
+						)
+					);
+				}
 				// Setup the minimum required user data
 				// TODO: Is null better than a random password?
 				// TODO: Look for otherMail, or proxyAddresses before UPN for email
@@ -385,12 +401,12 @@ class AADSSO {
 					'user_email' => $unique_name,
 					'user_login' => $unique_name,
 					'first_name' => $jwt->given_name,
-					'last_name'	=> $jwt->family_name,
-					'user_pass'	=> null
+					'last_name'  => $jwt->family_name,
+					'user_pass'  => null,
 				);
 
 				$new_user_id = wp_insert_user( $userdata );
-				
+
 				if ( is_wp_error( $new_user_id ) ) {
 					// The user was authenticated, but not found in WP and auto-provisioning is disabled
 					return new WP_Error(
@@ -400,11 +416,9 @@ class AADSSO {
 							$unique_name
 						)
 					);
-				}
-				else
-				{
+				} else {
 					AADSSO::debug_log( 'Created new user: \'' . $unique_name . '\', user id ' . $new_user_id . '.' );
-					$user = new WP_User( $new_user_id );					
+					$user = new WP_User( $new_user_id );
 				}
 			} else {
 
